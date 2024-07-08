e7eafc642c6849510c10dc3214ef93ed47c880f9
DavidRico <davidrico.dev@gmail.com>
2024-07-08 12:37:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Spring Framework, Interceptores HTTP horario acceso cliente
+
+En este proyecto veremos un nuevo ejemplo de interceptores HTTP con horarios de acceso para clientes
+
+## Contenidos
+
+- Crear interceptor horario.
+- Crear método handler cerrado.
+- Registrar interceptor.
+- Revisar errores NullPointerException.
---
+++ src/main/java/es/davidrico/springboot/horariointerceptor/interceptors/HorarioInterceptor.java
@@ -0,0 +1,59 @@
+package es.davidrico.springboot.horariointerceptor.interceptors;
+
+import java.util.Calendar;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component("horario")
+public class HorarioInterceptor implements HandlerInterceptor {
+	
+	@Value("${config.horario.apertura}")
+	private Integer apertura;
+	
+	@Value("${config.horario.cierre}")
+	private Integer cierre;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		Calendar calendar = Calendar.getInstance();
+		int hora = calendar.get(Calendar.HOUR_OF_DAY);
+		
+		if(hora >= apertura && hora < cierre) {
+			
+			StringBuilder mensaje = new StringBuilder("Bienvenido al horario de atención al cliente, atendemos desde las ");
+			mensaje.append(apertura);
+			mensaje.append(" horas");
+			mensaje.append(", hasta las ");
+			mensaje.append(cierre);
+			mensaje.append(" horas. Gracias por su visita.");
+			request.setAttribute("mensaje", mensaje.toString());
+			return true;
+		}
+
+		response.sendRedirect(request.getContextPath().concat("/cerrado"));
+		return false;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+		String mensaje = (String) request.getAttribute("mensaje");
+		
+		if(modelAndView != null && handler instanceof HandlerMethod) {
+			
+		modelAndView.addObject("horario", mensaje);
+		}
+	}
+
+	
+}
